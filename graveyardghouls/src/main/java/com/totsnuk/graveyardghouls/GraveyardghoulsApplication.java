573bd94ec86cf3c6775c451bf16dc682b2eb4fa3
tot121212 <git@github.com:tot121212>
2026-02-04 14:23:57 -0600

    feat: initializer java 21, maven

---
+++ graveyardghouls/src/main/java/com/totsnuk/graveyardghouls/GraveyardghoulsApplication.java
@@ -0,0 +1,13 @@
+package com.totsnuk.graveyardghouls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraveyardghoulsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraveyardghoulsApplication.class, args);
+	}
+
+}
---
+++ graveyardghouls/src/test/java/com/totsnuk/graveyardghouls/GraveyardghoulsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totsnuk.graveyardghouls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraveyardghoulsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
