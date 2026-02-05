6fecb02d15e6fd97a6186d8a20217850a599c5c7
tot121212 <git@github.com:tot121212>
2026-02-05 02:26:27 -0600

    refactor: reformat main method and add startup messages

--- graveyardghouls/src/main/java/com/totsnuk/graveyardghouls/GraveyardghoulsApplication.java
+++ graveyardghouls/src/main/java/com/totsnuk/graveyardghouls/GraveyardghoulsApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GraveyardghoulsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraveyardghoulsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraveyardghoulsApplication.class, args);
+        System.out.println("=== GRAVEYARD GHOULS ===");
+        System.out.println("Server started");
+        System.out.println("Press Ctrl+C to stop server");
+    }
 
 }
