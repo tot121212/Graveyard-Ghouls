8d30c06eadeda1ee8d995d186dfd5bea45cc897b
tot121212 <totmusicbusiness@gmail.com>
2026-06-21 15:00:59 -0500

    make enums  final

--- backend/src/main/java/com/totsnuk/graveyardghouls/enums/GameCookies.java
+++ backend/src/main/java/com/totsnuk/graveyardghouls/enums/GameCookies.java
@@ -1,6 +1,6 @@
 package com.totsnuk.graveyardghouls.enums;
 
-public class GameCookies {
+public final class GameCookies {
     public static final String PUBLIC_ID = "publicId";
     public static final String PRIVATE_TOKEN = "privateToken";
     public static final String GAME_SESSION_ID = "gameSessionId";
--- backend/src/main/java/com/totsnuk/graveyardghouls/enums/HTTPMode.java
+++ backend/src/main/java/com/totsnuk/graveyardghouls/enums/HTTPMode.java
@@ -1,6 +1,6 @@
 package com.totsnuk.graveyardghouls.enums;
 
-public class HTTPMode {
+public final class HTTPMode {
     // NOTE: change to true when using HTTPS
     public static final boolean USE_SECURE_PROTOCOL = false;
 }
